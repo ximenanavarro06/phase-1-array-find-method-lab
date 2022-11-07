6735b97b93630d8d2f67973508087118005e8d01
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-07 12:54:48 -0700

    commit tree b81c4efe82d5a340bc3db165d9afb62cbe428c8f

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"}
+]
+function superBowlWin(record){
+   const result = record.find(record => record.result === "W");
+   return result ? result.year : undefined;
+}
